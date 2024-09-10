f6e93a5837b411b2fa5b9ada856154bb2b8e2cdc
AbdulZor <abdulzor@outlook.com>
2024-09-10 12:30:02 +0300

    Finished removeFromArray assignment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+    // for each element in args
+    //   check indexOf arg element in arr until you cannot find one (when return of indexOf is 1)
+    //   if found, remove from index 1 element
+    for (const arg of args) {
+        let argIndex = arr.indexOf(arg);
+        while(argIndex > -1) {
+            arr.splice(argIndex, 1);
+            argIndex = arr.indexOf(arg);
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
