cb7096d228b5f23d056f7dfa838411cc7758fd81
AbdulZor <abdulzor@outlook.com>
2024-09-10 12:47:22 +0300

    Finished 05_sumAll assignment

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    // loop from start to end and add at each iteration the iteration number to the start.
+    // return start;
 
+    // start/end cannot be a negative integer, a floating point number, or not a number.
+    if((start < 0  || end < 0) || (Math.floor(start) !== start || Math.floor(end) !== end) || (start === NaN || end === NaN)) {
+        return "ERROR";
+    }
+
+    if (start > end) {
+        let tempEnd = start;
+        start = end;
+        end = tempEnd;
+    }
+
+    for(let i = start+1; i <= end; i++) {
+        // console.log(i);
+        start += i;
+    }
+    return start;
 };
 
 // Do not edit below this line
