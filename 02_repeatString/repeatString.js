b4e724005328ea6af21578b05ec265e21e2451e1
AbdulZor <abdulzor@outlook.com>
2024-09-10 11:09:20 +0300

    Finish 02_repeatString assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (str, repeatAmount) {
+    if (repeatAmount < 0) {
+        return "ERROR";
+    }
 
+    let newString = "";
+    for (let i = 0; i < repeatAmount; i++) {
+        newString += str;
+    }
+    return newString;
 };
 
 // Do not edit below this line
