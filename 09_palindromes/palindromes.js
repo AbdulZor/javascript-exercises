dd28d753d2a496346e8c24d857eae8742e4dfa03
AbdulZor <abdulzor@outlook.com>
2024-09-21 13:29:43 +0200

    Finish 09_palindromes assignment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const strLowercaseAndWithoutPunctuation = str.replace(/[^\w\s\']|_/g, "").replace(/\s+/g, "").toLowerCase().trim();
+    const arr = strLowercaseAndWithoutPunctuation.split('');
+    const reversedString = arr.reverse().join('');
+    return reversedString === strLowercaseAndWithoutPunctuation;
 };
 
 // Do not edit below this line
