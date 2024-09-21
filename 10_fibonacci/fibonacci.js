6ecde58cef053a7fafe97346c9a741f4da0ca93a
AbdulZor <abdulzor@outlook.com>
2024-09-21 13:42:28 +0200

    Finished 10_fibonacci assignment

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function (a) {
+    const arr = [0, 1];
+    if (a < 0) return "OOPS";
+    if (a == 0) return 0;
 
+    for (let i = 1; i < a; i++) {
+        arr.push(arr[i - 1] + arr[i]);
+    }
+    return arr.pop();
 };
 
 // Do not edit below this line
