5a5bc28a04b3ef8f3f69c040909b354464693802
AbdulZor <abdulzor@outlook.com>
2024-09-21 13:58:22 +0200

    Finished 12_findTheOldest assignment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+    return arr.sort((a, b) => {
+        if (a.yearOfDeath === undefined || b.yearOfDeath === undefined) {
+            return a.yearOfBirth < b.yearOfBirth ? 1 : -1;
+        }
 
+        const totalLiveda = a.yearOfDeath - a.yearOfBirth;
+        const totalLivedb = b.yearOfDeath - b.yearOfBirth;
+        return totalLiveda > totalLivedb ? 1 : -1;
+    }).pop();
 };
 
 // Do not edit below this line
